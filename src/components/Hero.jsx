fb25147c8ba4a0ea8f57f899903e22c725994003
chaerul anwar <chaerulanwar428@gmail.com>
2023-03-18 16:21:35 +0700

    menambah componen hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <div className='w-full h-screen'>
+        <img className="top-0 left-0 w-full h-screen object-cover" src="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg" alt="" />
+        <div className='bg-black/30 absolute top-0 left-0 w-full h-screen'/>
+        <div className='absolute top-0 w-full h-full flex flex-col justify-center text-white'>
+            <div className='md:left-[10%] max-w-[1100px] m-auto absolute p-4'>
+            <p>All Inclusive</p>
+            <h1 className='font-bold text-5xl md:text-7xl drop-shadow-2xl'>Private Beaches & Gateawy</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est illo, culpa sunt nemo nulla eaque ducimus iusto eveniet eos similique?</p>
+            <button className='bg-white text-black'>Reserve Now</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+
+export default Hero
