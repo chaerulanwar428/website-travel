d1e57878dd95e2605f0274eef72c5723fef279bc
chaerul anwar <chaerulanwar428@gmail.com>
2023-03-18 22:29:48 +0700

    menmabahkan components offers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Offers from "./components/Offers";
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Navbar/>
       <Hero/>
+      <Offers/>
     </div>
   );
 }
