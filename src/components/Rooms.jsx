9a0ae6781402de15e12089b8b54f16ed9028d239
chaerul anwar <chaerulanwar428@gmail.com>
2023-03-19 22:32:47 +0700

    menyelesaikan pembbuatan website

--- src/components/Rooms.jsx
+++ src/components/Rooms.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Rooms = () => {
   return (
-    <div className='max-w-[1400px] lg:h-[700px] bg-blue-100 mx-auto my-20 pt-16 lg:mb-[20%] md:mb-[35%] px-4 grid lg:grid-cols-3 gap-4 sm:h-[1200px]'>
+    <div className='max-w-[1400px] lg:h-[700px] sm:h-[1000px] bg-blue-100 mx-auto my-20 pt-16 lg:mb-[20%] md:mb-[35%] px-4 grid lg:grid-cols-3 gap-4 '>
         <div className='lg:top-20 relative lg:col-span-1 col-span-2'>
             <h3 className='text-2xl font-bold'>Ruang interior yang sangat elegan</h3>
         <p className='pt-4'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi, dolore itaque quibusdam libero vitae saepe.</p>
