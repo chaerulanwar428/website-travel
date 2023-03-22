afd4a63438b0b37275b33bf592127209bdd3b99b
chaerul anwar <chaerulanwar428@gmail.com>
2023-03-23 03:42:07 +0700

    menyesuaikan kembali

--- src/components/Rooms.jsx
+++ src/components/Rooms.jsx
@@ -10,7 +10,7 @@ const Rooms = () => {
         <div className='grid grid-cols-2 col-span-2 gap-2'>
             <img className='object-cover w-full h-full' src="https://images.unsplash.com/photo-1582719478250-c89cae4dc85b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
             <img className='row-span-2 object-cover w-full h-full'src="https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
-            <img className='object-cover w-full h-full' src="https://plus.unsplash.com/premium_photo-1678297269980-16f4be3a15a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
+            <img className='object-cover w-full h-full sm:hidden' src="https://plus.unsplash.com/premium_photo-1678297269980-16f4be3a15a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
         </div>
     </div>
   )
